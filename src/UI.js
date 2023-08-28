60322dd7ab1a98e7fd0e4fa9a2d4f81ddfd2f42e
KyleOlhausen <olhausen.kyle43@gmail.com>
2023-08-28 16:42:57 -0400

    added details popup, not yet styled

--- src/UI.js
+++ src/UI.js
@@ -184,39 +184,78 @@ function loadProject(projName) {
         taskleft.appendChild(checkboxLabel);
 
         //when checkbox clicked toggle checked status and save
-        checkbox.addEventListener('click', () => {
+        checkbox.addEventListener('click', () => { //add to event listener at bottom?
             task.setChecked(checkbox.checked);
             saveProject(currProj);
         });
         
         
 
-
-      
-
+        //create description button, date, edit button, delete button (taskright)
         const taskright = document.createElement('div');
         taskright.classList.add('task-right');
         //taskright.innerHTML += `<input type="date" class="input-due-date-${i}">`;
 
         const taskEdit = document.createElement('div');
         taskEdit.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5,3C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19H5V5H12V3H5M17.78,4C17.61,4 17.43,4.07 17.3,4.2L16.08,5.41L18.58,7.91L19.8,6.7C20.06,6.44 20.06,6 19.8,5.75L18.25,4.2C18.12,4.07 17.95,4 17.78,4M15.37,6.12L8,13.5V16H10.5L17.87,8.62L15.37,6.12Z" /></svg>`
-        const taskDelete = document.createElement('div');
-        taskDelete.innerHTML = `<svg class="task-close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_MD"> <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>`
+        taskEdit.classList.add('task-edit');
+        // const taskDelete = document.createElement('div');
+        // taskDelete.innerHTML = `<svg class="task-close" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Menu / Close_MD"> <path id="Vector" d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>`
+        const taskDelete = document.createElement('button');
+        taskDelete.textContent = "X";
+        taskDelete.classList.add('task-close');
 
-        taskright.appendChild(taskEdit);
-        taskright.appendChild(taskDelete);
+        const taskDetails = document.createElement('button');
+        taskDetails.textContent = "Details"
+        taskDetails.classList.add('task-details');
+
+        const taskDate = document.createElement('div');
+        taskDate.classList.add('task-date');
+        taskDate.textContent = task.getDueDate();
+
+
+
+        taskright.appendChild(taskDetails);
+        // taskright.appendChild(taskEdit);
+        // taskright.appendChild(taskDelete);
         newTaskLi.appendChild(taskleft);
         newTaskLi.appendChild(taskright);
 
         newTaskLi.addEventListener('click', e => {
             const target = e.target;
             if(target.classList[0] === 'task-close'){
+                //taskname probably off by 1 parentelement
                 let taskname = target.parentElement.parentElement.previousElementSibling.textContent;
 
                 deleteTask(taskname, currProj);
                 saveProject(currProj);
                 loadProject(currProj.getName());
             }
+            else if (target.classList[0] === 'task-details') {
+                //open details popup
+                const taskDetailsPopup = document.querySelector('.task-details-popup');
+                taskDetailsPopup.showModal();
+
+                //fill out detail popup
+                const detailsTitle = document.querySelector('.task-popup-title');
+                detailsTitle.textContent = task.getName();
+                const detailsDescription = document.querySelector('.task-popup-description');
+                detailsDescription.textContent = task.getDescription();
+                const detailsDate = document.querySelector('.task-popup-date');
+                detailsDate.textContent = task.getDueDate();
+                const detailsPriority = document.querySelector('.task-popup-priority');
+                detailsPriority.textContent = task.getPriority();
+
+                //close details popup when button clicked
+                const closeDetailsPopup = document.querySelector('.close-details-popup');
+                closeDetailsPopup.addEventListener('click', () => {
+                    taskDetailsPopup.close();
+                });
+                
+            }
+            // else if (target.classList[0] === 'task-edit') {
+
+            // }
         });
 
         tasklist.appendChild(newTaskLi);
